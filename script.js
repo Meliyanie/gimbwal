031cbce32c99dfcbec9f3f9e3b5f0ee648fd8d57
Meliyanie <167956841+Meliyanie@users.noreply.github.com>
2025-03-06 14:47:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ window.addEventListener('load', () => {
 
 const messages = [
     {
-        text: "plss mau dong",
+        text: "plss you will do again?",
         image: "https://i.pinimg.com/736x/1e/7c/6f/1e7c6ff7a0c26f7eaefbce683656193d.jpg"
     },
     {
-        text: "pls klik yg kiri",
+        text: "pls Click the left",
         image: "https://i.pinimg.com/736x/90/73/a9/9073a9359bff531cd830ae7384752934.jpg"
     },
     {
-        text: "terakhir, kalo \"no\" yauda deh",
-        image: "https://i.pinimg.com/736x/a6/43/f5/a643f5a40db387585b56bc767b66fddb.jpg"
+        text: "last, if \"no\" its fine",
+        image: "https://pin.it/6z31ugMWo"
     }
 ];
 
